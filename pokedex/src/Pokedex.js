33d2de641a18ea9310f63bc5397b360b2248fa61
farhanbinamjad <farhanbinamjad@gmail.com>
2021-05-06 12:04:18 -0400

    pokedex

---
+++ pokedex/src/Pokedex.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Pokecard from'./Pokecard';
+import './Pokedex.css';
+
+export class Pokedex extends Component {
+
+
+
+    render() {
+        let title;
+        if (this.props.isWinner){
+            title= <h1 className="Pokedex-winner">WINNER</h1>
+        } else{
+            title= <h1 className="Pokedex-loser">LOSER</h1> 
+        }
+        return (
+            <div className="Pokedex">
+                <h1>Pokedex!</h1>
+                <p>Total Experience: {this.props.exp}</p>
+                {title}
+                <div className='Pokedex-cards'>
+                {this.props.pokemon.map((p) => (
+                    <Pokecard id={p.id} name={p.name} type= {p.type} exp={p.base_experience}/>
+                ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Pokedex
