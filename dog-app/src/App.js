75ac3112717e0024db67b34878d8ee33dd42dee1
farhanbinamjad <farhanbinamjad@gmail.com>
2021-05-10 13:11:58 -0400

    dog-app

---
+++ dog-app/src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import whiskey from "./whiskey.jpg";
+import tubby from "./tubby.jpg";
+import hazel from "./hazel.jpg";
+import "./App.css";
+import Navbar from "./Navbar";
+import Routes from "./Routes";
+
+
+class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: whiskey,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: hazel,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  };
+  render() {
+
+    return(
+      <div>
+        <Navbar dogs={this.props.dogs}/>
+        <div className="container">        
+        <Routes dogs={this.props.dogs} />
+        </div>
+      </div>
+    )
+    }
+}
+
+export default App;
