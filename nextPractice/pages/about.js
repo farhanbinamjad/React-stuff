c72757680aa901e7231d4ddaba4169d01194d59d
farhanbinamjad <farhanbinamjad@gmail.com>
2021-05-11 11:30:44 -0400

    next js practice

---
+++ nextPractice/pages/about.js
@@ -0,0 +1,6 @@
+const about = () => {
+    return(
+        <h1>About!</h1>
+    )
+}
+export default about;
---
+++ nextPractice/pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const index = () => (
+    <div>
+    <Link href="/about">
+    <a>About</a>
+    </Link>
+        <h1>Index!</h1>
+    </div>
+);
+
+export default index;
